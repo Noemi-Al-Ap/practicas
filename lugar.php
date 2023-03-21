797ff28554c22daef89f27ae0ce72ce8a930e81a
Noemi A.A <107079563+Noemi-Al-Ap@users.noreply.github.com>
2023-03-21 13:44:43 -0600

    Add files via upload

---
+++ lugar.php
@@ -0,0 +1,70 @@
+<?php
+ 'include/conexion.php';
+       'include/action.phps';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="stylesheet" type="text/css " href="css/bootstrap.min.css">
+    <title>LUGARES (registro)</title>
+</head>
+<body>
+<div class="container m-0 content-justify-center py-2">
+    <div class ="row text-center">
+        <h1 class="text-center">Lugares</h1>
+    </div>
+<form id="Registro" Action="<?php echo $_SERVER['php_SELF'];?>"
+Method="Post">
+<div class="row text-center">
+    <div class="Col-sm-12 col-md-12 Col-lg-12">
+        <input type="text" name="Nombre Lugar" class="Form-control" placeholder="Nombre Lugar" required>
+
+    </div>
+</div>
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="text" name="Descripcion" class="Form-control" placeholder="Descripcion" required>
+
+    </div>
+</div>
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="tel" name="Telefono" class="Form-control" placeholder="55-55-55-55-55" required>
+
+    </div>
+</div>
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="Email" name="Email" class="Form-control" placeholder="Email" required>
+
+    </div>
+</div>
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="text" name="Facebook" class="Form-control" placeholder="Facebook" required>
+
+    </div>
+</div>
+
+
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="text" name="Horario" class="Form-control"  placeholder="Horario" required>
+
+    </div>
+</div>
+
+</form>
+<div class="row text-center">
+    <div class="Col-sm-12 Col-md-12 Col-lg-12">
+        <input type="Submit" value="Registrar" name="btn" class=" btn btn-success" >
+
+    </div>
+</div>
+
+</body>
+</html>
+
