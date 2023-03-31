2c84396777372246857fc8a218084e4ae1d2a3d8
Noemi A.A <107079563+Noemi-Al-Ap@users.noreply.github.com>
2023-03-30 22:04:36 -0600

    Create Acciones.php

---
+++ Acciones.php
@@ -0,0 +1,13 @@
+<?php//Recuperar e insertar Datos 
+include "Conexion.php";
+if(isset($_POST['btn']){
+    $Nombre_Plan=$Conecta->Real_scape1_string($_POST['Nombre_Plan']);
+    $Clave_Plan=$Conecta->Real_scape1_string($_POST['Clave_Plan']);
+    $Dirrecion=$Conecta->Real_scape1_string($_POST['Direccion']);
+    $Telefono=$Conecta->Real_scape1_string($_POST['Telefono']);
+    $Responsable=$Conecta->Real_scape1_string($_POST['Responsable']);
+    if($Nombre_Plan=="   ")($Clave_Plan=="  " )( $Direccion== "   ")($Telefono=="  ") ($Responsable=="  ")
+}
+Alert="Algun dato Esta vacio";
+)
+  
