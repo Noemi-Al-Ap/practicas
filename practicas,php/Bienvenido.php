d490dce927e744c2f1e447c7792de83dec6c51e9
Noemi A.A <107079563+Noemi-Al-Ap@users.noreply.github.com>
2023-04-21 00:23:38 -0600

    Add files via upload

---
+++ practicas,php/Bienvenido.php
@@ -0,0 +1,12 @@
+<?php
+   echo "BIENVENIDOS AL CURSO DE PHP";
+   echo"<br>";
+   echo"            <br>";
+   echo "Modulo:Diseño de Desarrollo de Aplicasiones <br>";
+   
+   echo"Profesor:Jose Luis Chaves Gomez<br>";
+
+   echo" Alumna:  Alcauter Aponte Noemi<br>";
+  
+   echo" Semetre:SEXTO Grupo:605        <BR>";
+?>
